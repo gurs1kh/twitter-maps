d6f968088a41a8711728229b36d6c0f71de92bf2
Ian Durra <Thaddeus@Ians-MacBook-Pro-3.local>
2015-07-22 04:11:17 -0700

    404 Errors on adding Script

--- js/query.js
+++ js/query.js
@@ -1,16 +1,79 @@
-var queryApp = angular.module('queryApp', [])
+/*
+    INFO 343
 
 
-queryApp.controller('queryCtrl', function($scope){
+var baseUrl = "https://api.twitter.com/1.1/search/tweets.json?q=";
+var country = "";
+var favorite = "";
+var retweet = "";
+var location = false;
+var language = false;
+var date = false;
+var queryApp = angular.module('queryApp', []);
+
+var queryCtrl = queryApp.controller('queryCtrl', function($scope, $http){
+});
+
     //url encode this
     $scope.query = "";
     $scope.latitude = "";
     $scope.longitude = "";
+    $scope.radius = "";
     $scope.langs = "";
     $scope.country = "";
-    $scope.date = "";
+    $scope.date = "2015-07-";
     $scope.favorite = "";
     $scope.retweet = "";
+    $scope.locationShow = false;
+    $scope.langsShow = false;
+    $scope.countryShow = false;
+    $scope.dateShow = false;
+    $scope.favoriteShow = false;
+    $scope.retweetShow = false;
+    $scope.miles = ["1", "5", "10", "20", "50", "100", "1000", "10,000"];
 
+    $scope.isDisabled = function(a,b,c,d,e,f){
+    	return (a ||
+    		$scope.locationShow && b || 
+    		$scope.locationShow && c ||
+    		$scope.dateShow && d || 
+    		$scope.favoriteShow && e ||
+    		$scope.retweetShow && f);
+    }
+
+   	$scope.ajaxCall = function(){
+   		var query = $scope.query;
+
+   		//form query
+   		if($scope.locationShow){
+   			var location = true;
+   			query += "&geocode=" + $scope.latitude + "," + 
+   				$scope.longitude + "," + $scope.radius + "mi";
+   		}
+   		if($scope.langsShow){
+   			var language = true;
+   			query += "&lang=" + $scope.langs;
+   		}
+   		if($scope.dateShow){
+   			var date = true;
+   			query += "&until=" + $scope.date;
+   		}
+
+   		//expose desired variables
+   		if($scope.countryShow){
+   			country = $scope.country;
+   		}
+   		if($scope.favoriteShow){
+ 			favorite = $scope.favorite;
+   		}
+   		if($scope.retweetShow){
+   			retweet = $scope.retweet;
+   		}
+
+   		var formattedURL = encodeURIComponent(query);
+   		$http.get(baseUrl + formattedURL).success(function(response){
+            console.log(response);
+        });
+   	}
 })
-
+*/
