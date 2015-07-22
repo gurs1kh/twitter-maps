7c4d3559c10da013eae5d2def80f7b68b63880a1
gurs1kh <gurs1kh@live.com>
2015-07-21 22:06:43 -0700

    some more changes to maps.html

--- js/twitter-maps.js
+++ js/twitter-maps.js
@@ -1,4 +1,5 @@
 var map;
+var radius;
 
 $(function() {
 	map = L.map("map");
@@ -14,58 +15,77 @@ function search(query) {
 		 type: "get",
 		 dataType:"json",
 		 success: function(data) {
-			populateMap(data, map);
+			populateMap(data);
 		 }
 	});
 };
 
-function populateMap(data, map) {
+function populateMap(data) {
 	var circles = new Array();
 	var count = 0;
+	radius = 100000;
 	data.statuses.map(function(d){
 		if (d.geo && d.geo.coordinates) {
-			var circle = new L.circle(d.geo.coordinates, 10000, {color:'#55acee', fillColor:'white'}).addTo(map);
+			var circle = new L.circle(d.geo.coordinates, radius, {color:'#55acee', fillColor:'white', fillOpacity:0.8, weight:8}).addTo(map);
 			circles.push(circle);
 			circle.selected = false;
-			var info;
 			if (count < 5) {
-				//var circle2 = new L.circle(d.geo.coordinates, 1000000, {color:'red'}).addTo(map);
+				//var circle2 = new L.circle(d.geo.coordinates, 300000, {color:'red'}).addTo(map);
+				var info;
 				$.ajax({
-					 url:'services/getEmbeddedTweet.php',
-					 data: {id : d.id_str},
-					 type: "get",
-					 dataType:"json",
-					 success: function(tweet) {
+					url:'services/getEmbeddedTweet.php',
+					data: {id : d.id_str},
+					type: "get",
+					dataType:"json",
+					success: function(tweet) {
 						info = tweet.html;
 						console.log(tweet);
 						circle.bindPopup(info);
-					 }
+					}
 				});
 			}
 			count++;
 			
 			var popup = L.popup()
-				.setLatLng([d.geo.coordinates[0] + 1, d.geo.coordinates[1]])
+				.setLatLng([d.geo.coordinates[0] + 0.5, d.geo.coordinates[1]])
 				.setContent(d.user.name);
-				
-			var popup2 = L.popup()
-				.setLatLng([d.geo.coordinates[0] + 1, d.geo.coordinates[1]])
-				.setContent(info);
 			
 			circle.on("click", function(e) {
-				$("#info").html(info);
-				popup2.openOn(map);
-				circles.map(function(c) { c.setRadius(10000); c.selected = false; });
-				circle.setRadius(100000);
+				$('#info').html(info);
+				circles.forEach(function(c) { c.setRadius(radius); c.selected = false; });
+				circle.setRadius(radius * 3);
 				circle.selected = true;
 			}).on('mouseover', function (e) {
-				circle.setRadius(100000);
+				circle.setRadius(radius * 3);
 				popup.openOn(map);
 			}).on('mouseout', function (e) {
-				circle.setRadius(e.target.selected ? 100000 : 10000);
+				circle.setRadius(e.target.selected ? radius * 3 : radius);
 				popup._close();
-
 			});
+		}
+	});
+	handleZoom(circles);
+}
+
+function handleZoom(circles) {
+	var myZoom = {
+	  start:  map.getZoom(),
+	  end: map.getZoom()
+	};
+
+	map.on('zoomstart', function(e) {
+	   myZoom.start = map.getZoom();
+	});
+
+	map.on('zoomend', function(e) {
+		myZoom.end = map.getZoom();
+		var diff = myZoom.start - myZoom.end;
+		if (diff > 0) {
+			radius *= 2;
+			circles.forEach(function(circle) {  circle.setRadius(circle.getRadius() * 2); });
+		} else if (diff < 0) {
+			radius /= 2;
+			circles.forEach(function(circle) { circle.setRadius(circle.getRadius() / 2); });
 		}
 	});
 }
