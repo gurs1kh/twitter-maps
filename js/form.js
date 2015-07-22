a935472f9ae10ddd2d9fbbe817c1a6e743e0b023
gurs1kh <gurs1kh@live.com>
2015-07-22 15:30:06 -0700

    a

--- js/form.js
+++ js/form.js
@@ -1,78 +1,59 @@
 var formApp = angular.module('formApp', []);
 
-var baseUrl = "https://api.twitter.com/1.1/search/tweets.json?q=";
 var country = "";
-var favorite = "";
-var retweet = "";
+var favorites = "";
+var retweets = "";
 var locationShow = false;
 var language = false;
 var date = false;
 
-formApp.controller('FormController', function($scope, $http){
+formApp.controller('formCtrl', function($scope, $http){
 	//url encode this
-    $scope.query = "";
-    $scope.latitude = "";
-    $scope.longitude = "";
-    $scope.radius = "";
-    $scope.langs = "";
-    $scope.country = "";
-    $scope.date = "";
-    $scope.favorite = "";
-    $scope.retweet = "";
-    $scope.locationShow = false;
-    $scope.langsShow = false;
-    $scope.countryShow = false;
-    $scope.dateShow = false;
-    $scope.favoriteShow = false;
-    $scope.retweetShow = false;
-    $scope.miles = ["1", "5", "10", "20", "50", "100", "1000", "10,000"];
-    $scope.languages = ["en", "ar"];
-    $scope.countries = ["us", "ar"];
+	$scope.query = null;
+	$scope.latitude = null;
+	$scope.longitude = null;
+	$scope.radius = null;
+	$scope.lang = null;
+	$scope.country = null;
+	$scope.date = null;
+	$scope.favorites = null;
+	$scope.retweets = null;
+	$scope.locationShow = false;
+	$scope.langShow = false;
+	$scope.countryShow = false;
+	$scope.dateShow = false;
+	$scope.favoritesShow = false;
+	$scope.retweetsShow = false;
+	$scope.miles = ["1", "5", "10", "20", "50", "100", "1000", "10,000"];
+	$scope.languages = ["en", "ar"];
+	$scope.countries = ["us", "uk", "ar"];
 
-    $scope.isDisabled = function(a,b,c,d,e,f){
-      return (a ||
-        $scope.locationShow && b || 
-        $scope.locationShow && c ||
-        $scope.dateShow && d || 
-        $scope.favoriteShow && e ||
-        $scope.retweetShow && f);
-    }
+	$scope.isDisabled = function(a,b,c,d,e,f){
+	  return (a ||
+		$scope.locationShow && b || 
+		$scope.locationShow && c ||
+		$scope.dateShow && d || 
+		$scope.favoritesShow && e ||
+		$scope.retweetsShow && f);
+	}
 
-    $scope.ajaxCall = function(){
-      var query = $scope.query;
-
-      //form query
-      if($scope.locationShow){
-        var locationShow = true;
-        query += "&geocode=" + $scope.latitude + "," + 
-          $scope.longitude + "," + $scope.radius + "mi";
-      }
-      if($scope.langsShow){
-        var language = true;
-        query += "&lang=" + $scope.langs;
-      }
-      if($scope.dateShow){
-        $scope.date = "2015-07-" + $scope.date;
-        var date = true;
-        query += "&until=" + $scope.date;
-      }
-
-      //expose desired variables
-      if($scope.countryShow){
-        country = $scope.country;
-      }
-      if($scope.favoriteShow){
-      favorite = $scope.favorite;
-      }
-      if($scope.retweetShow){
-        retweet = $scope.retweet;
-      }
-
-      
-      var formattedURL = encodeURIComponent(query);
-      $http.get(baseUrl + formattedURL).success(function(response){
-            console.log(response);
-      });
-      
-    }
+	$scope.submitSearch = function(){
+		var radius = null;
+		if ($scope.radius)
+			radius = $scope.radius + "mi";
+		
+		var query = {
+			q: $scope.query,
+			location: {
+				coordinates: [$scope.latitude, $scope.longitude],
+				radius: radius
+			},
+			lang: $scope.lang,
+			date: $scope.date,
+			country: $scope.country,
+			retweets: $scope.retweets,
+			favorites: $scope.favorites
+		}
+		search(query);
+	}
 });
--- js/twitter-maps.js
+++ js/twitter-maps.js
@@ -1,4 +1,5 @@
 var map;
+var circles;
 var radius;
 
 $(function() {
@@ -6,7 +7,7 @@ $(function() {
 	map.setView([40, 30], 2);
 	var layer = L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(map);
 	var query = {
-		q: "api",
+		q: "the",
 		location: {
 			coordinates: [0, 0],
 			radius: "4000mi"
@@ -21,11 +22,22 @@ $(function() {
 });
 
 function search(query) {
+	console.log(query);
+	if (circles) {
+		circles.forEach(function(circle) {
+			map.removeLayer(circle);
+		});
+		circles = null;
+	}
+	
 	var geocode;
-	if (query.location && query.location.coordinates && query.location.radius)
+	
+	if (query.location && query.location.coordinates && query.location.radius) {
 		geocode = query.location.coordinates[0] + "," 
 				+ query.location.coordinates[1] + ","
 				+ query.location.radius;
+		if (geocode.substring(0,2) == ",,") geocode = null;
+	}
 	$.ajax({
 		 url:'../services/getData.php',
 		 data: {q: query.q, geocode: geocode, lang: query.lang, until: query.date},
@@ -38,17 +50,19 @@ function search(query) {
 };
 
 function populateMap(data, query) {
-	var circles = new Array();
+	circles = new Array();
+	console.log(data);
 	var count = 0;
 	radius = 100000;
 	data.statuses.map(function(d){
 		if (d.geo && d.geo.coordinates
 		 && d.place && (!query.country || (query.country && d.place.country_code == query.country))
-		 && d.retweet_count >= query.retweets && d.favorite_count >= query.favorites) {
+		 && (!query.retweets || (query.retweets && d.retweet_count >= query.retweets))
+		 && (!query.favorites || (query.favorites && d.favorite_count >= query.favorites))) {
 			var circle = new L.circle(d.geo.coordinates, radius, {color:'#55acee', fillColor:'white', fillOpacity:0.8, weight:5}).addTo(map);
 			circles.push(circle);
 			circle.selected = false;
-			if (count < 5) {
+			if (count < 1) {
 				var info;
 				$.ajax({
 					url:'../services/getEmbeddedTweet.php',
